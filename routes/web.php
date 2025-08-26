8ab79f77f93cd6735e8e1e963091357ac3583829
Muhammad Akmal <makmal316@gmail.com>
2025-08-26 20:44:11 +0700

    fix: make list categories public

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,19 @@ Route::post('users/signin', [AuthController::class, 'signIn']);
 
 Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
 Route::get('products', [ProductController::class, 'index']);
+Route::get('categories', [CategoryController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function (): void {
     Route::get('users/me', [AuthController::class, 'current']);
     Route::patch('users/me', [AuthController::class, 'update']);
     Route::delete('users/signout', [AuthController::class, 'signOut']);
 
-    Route::apiResources([
-        'users' => UserController::class,
-        'categories' => CategoryController::class,
-    ]);
+    Route::apiResource('users', UserController::class);
+
+    Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::patch('categories/{categories}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('categories/{categories}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    Route::get('categories/{categories}', [CategoryController::class, 'show'])->name('categories.show');
 
     Route::post('products', [ProductController::class, 'store'])->name('products.store');
     Route::patch('products/{product}', [ProductController::class, 'update']);
