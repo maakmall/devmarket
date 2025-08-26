1ec72efb248fc71379ae616cedf3de1560aedf9a
Muhammad Akmal <makmal316@gmail.com>
2025-08-26 12:23:44 +0700

    feat: add category management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\{AuthController, UserController};
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    AuthController,
+    CategoryController,
+    UserController
+};
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,8 +13,14 @@ Route::get('/', function () {
 
 Route::post('users/signup', [AuthController::class, 'signUp']);
 Route::post('users/signin', [AuthController::class, 'signIn']);
-Route::get('users/me', [AuthController::class, 'current'])->middleware('auth:sanctum');
-Route::patch('users/me', [AuthController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('users/signout', [AuthController::class, 'signOut'])->middleware('auth:sanctum');
 
-Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function (): void {
+    Route::get('users/me', [AuthController::class, 'current']);
+    Route::patch('users/me', [AuthController::class, 'update']);
+    Route::delete('users/signout', [AuthController::class, 'signOut']);
+
+    Route::apiResources([
+        'users' => UserController::class,
+        'categories' => CategoryController::class,
+    ]);
+});
